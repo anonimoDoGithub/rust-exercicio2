b76b5d96668c97637c891770b44d2803a56b97cf
anonimoDoGithub <xxexpgamerxx@gmail.com>
2023-11-13 06:55:25 -0300

    exercício2 - feito

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 
-fn convert_to_int(data_input: &String) -> i32 {
-    let x = data_input.trim().parse::<i32>().unwrap();
+fn convert_to_int(data_input: &String) -> u32 {
+    let x = data_input.trim().parse::<u32>().unwrap();
     x
 }
 
