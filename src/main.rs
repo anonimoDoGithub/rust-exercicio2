128c85bb0d147cde65c2e6d98b3533c69a0319fc
anonimoDoGithub <xxexpgamerxx@gmail.com>
2023-11-13 06:45:04 -0300

    exercício2 - feito

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
+fn convert_to_int(data_input: &String) -> i32 {
+    let x = data_input.trim().parse::<i32>().unwrap();
+    x
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut number1 = String::new();
+    io::stdin().read_line(&mut number1).expect("error reading the number");
+
+    if convert_to_int(&number1) % 2 == 0{
+        println!("the number {} is even!", number1);
+    }else {
+        println!("the number {} is odd!!", number1);
+    }
 }
